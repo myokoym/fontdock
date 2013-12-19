d787dd46feb9026833768edb772f1a2a9b9e4b47
Masafumi Yokoyama <myokoym@gmail.com>
2013-12-20 01:27:22 +0900

    Remove an environment setting
    Because I'm not want to use "ENV".

--- lib/fontdock/command.rb
+++ lib/fontdock/command.rb
@@ -5,7 +5,6 @@ module Fontdock
   class Command < Thor
     desc "ls", "List available font families"
     def ls
-      ENV["LANG"] = "C"
       families = Pango::CairoFontMap.default.families
       names = families.collect {|family| family.name }
       names.sort.uniq.each {|name| puts name }
